5bf893ba0e1e5dc1047cbe887cc01b412f79dffb
Omeleto <90705482+OmeletoTheEgg@users.noreply.github.com>
2022-07-14 18:17:46 +0800

    "HAVE THE COPY KEYFRAMES TO NEW ACTION THING WORKING! Thanks to pose library built in add-on"

--- __init__.py
+++ __init__.py
@@ -13,6 +13,9 @@ bl_info = {
 from typing import Optional, FrozenSet, Set, Union, Iterable, cast
 import bpy
 
+
+
+
 # class PoseCreationParams:
 #     armature_ob: bpy.types.Object
 #     src_action: Optional[bpy.types.Action]
@@ -55,7 +58,7 @@ import bpy
 #             bpy.data.actions.remove(dst_action)
 
 class CreateAnimationAsset(bpy.types.Operator):
-    bl_idname = "object.create_animation_asset"
+    bl_idname = "pose.create_animation_asset"
     bl_label = "Create Animation Asset test"
     bl_description = (
         "Creates an Action that contains the selected keyframes of the selected bones, marks it as an asset"
@@ -68,16 +71,34 @@ class CreateAnimationAsset(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context: bpy.types.Context) -> Set[str]:
-        dst_action = bpy.data.actions.new("test")
-        src_obj = bpy.context
-        for action in src_obj.actions:
-            for fcurve in action.fcurves:
-                src_keyframe_points = fcurve.keyframe_points
-                dst_action.fcurves.new()
+        new_action = bpy.data.actions.new("test")
+        src_action = bpy.context.object.animation_data.action
+        bone_names = {bone.name for bone in bpy.context.selected_pose_bones_from_active_object}
+        for bone_name in sorted(bone_names):
+            for location_index in range(3):
+                bone = bpy.context.object.pose.bones[bone_name]
+                rna_path = bone.path_from_id("location")
+                new_fcurve = new_action.fcurves.find(rna_path, index=location_index)
+                if new_fcurve is None:
+                    new_fcurve = new_action.fcurves.new(rna_path, index=location_index, action_group=bone_name)
+                src_fcurve = src_action.fcurves.find(rna_path, index=location_index)
+                for keyframe in src_fcurve.keyframe_points:
+                    new_fcurve.keyframe_points.insert(frame=keyframe.co.x, value=keyframe.co.y)
+
+                
+
+ 
+
+        # for fcurve in src_obj.action.fcurves:
+        #     src_keyframe_points = fcurve.keyframe_points
+        #     print(src_keyframe_points)
 
                 # I need to figure out how to create fcurves from dst_action the same data path as each fcurve of src_action
                 # probably need to look at what Pose Library did on that, but i need to ensure that I'm getting the keyframe points themselves,
                 # not the value of the fcurve in the specific time which what Pose Library does
+
+                # Maybe try having only operators to copy from existing action to new action.
+                # But the problem with that is the new action doesn't initially have the same fcurves as the existing action
                 
         return {'FINISHED'}
 
@@ -86,11 +107,11 @@ def menu_func(self, context):
 
 def register():
     bpy.utils.register_class(CreateAnimationAsset)
-    bpy.types.VIEW3D_MT_object.append(menu_func)
+    bpy.types.VIEW3D_MT_pose.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(CreateAnimationAsset)
-    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    bpy.types.VIEW3D_MT_pose.remove(menu_func)
 
 if __name__ == "__main__":
     register()
