1f40da07df500afe6b7b99ea81f527f043a30e5c
Omeleto <90705482+OmeletoTheEgg@users.noreply.github.com>
2022-07-17 17:42:19 +0800

    Tried editing using vim

--- __init__.py
+++ __init__.py
@@ -13,11 +13,46 @@ bl_info = {
 import inspect
 from typing import Optional, FrozenSet, Set, Union, Iterable, cast
 import bpy
+from bpy import [
+        Action,
+        Operator
+]
 
-# Do the exact same way like the createanimationasset does
-# Or find a way to copy and paste directly
+class CreateAnimationAsset(Operator):
+    bl_idname = "pose.create_animation_asset"
+    bl_label = "Create Animation Asset"
+    bl_description = (
+        "Creates an Action that contains the selected keyframes of the selected bones, marks it as an asset"
+    )
+    bl_options = {"REGISTER", "UNDO"} 
 
-class ApplyAnimationAsset(bpy.types.Operator):
+
+    @classmethod
+    def poll(cls, context: Operator) -> bool:
+        return context.active_object is not None
+
+    def execute(self, context: Operator) -> Set[str]:
+        new_action = bpy.data.actions.new("Asset")
+        src_action = context.object.animation_data.action
+        bone_names = {bone.name for bone in bpy.context.selected_pose_bones_from_active_object}
+        for bone_name in sorted(bone_names):
+            for location_index in range(3):
+                bone = bpy.context.object.pose.bones[bone_name]
+                rna_path = bone.path_from_id("location")
+                new_fcurve = new_action.fcurves.find(rna_path, index=location_index)
+                if new_fcurve is None:
+                    new_fcurve = new_action.fcurves.new(rna_path, index=location_index, action_group=bone_name)
+                src_fcurve = src_action.fcurves.find(rna_path, index=location_index)
+                for keyframe in src_fcurve.keyframe_points:
+                    new_fcurve.keyframe_points.insert(frame=keyframe.co.x, value=keyframe.co.y)
+                
+                new_action.asset_mark()
+                new_action.asset_generate_preview()
+        
+        return {'FINISHED'}
+
+
+class ApplyAnimationAsset(Operator):
     bl_idname = "pose.apply_animation_asset"
     bl_label = "Apply Animation Asset"
     bl_description = (
@@ -25,8 +60,8 @@ class ApplyAnimationAsset(bpy.types.Operator):
     )
     bl_options = {"REGISTER", "UNDO"}
 
-    # def poll(cls, context: bpy.types.Context) -> bool:
-    #     return context.active_object is not None
+    def poll(cls, context: bpy.types.Context) -> bool:
+        return context.active_object is not None
 
     def execute(self, context: bpy.types.Context) -> Set[str]:
         
@@ -49,51 +84,12 @@ class ApplyAnimationAsset(bpy.types.Operator):
 
                 for keyframe in from_fcurve.keyframe_points:
                     target_fcurve.keyframe_points.insert(frame=keyframe.co.x + frame_current, value=keyframe.co.y)
-
                 
-
         # print(smallest_x)
 
-        
-                
-                
-
         return {'FINISHED'}
         
 
-
-class CreateAnimationAsset(bpy.types.Operator):
-    bl_idname = "pose.create_animation_asset"
-    bl_label = "Create Animation Asset"
-    bl_description = (
-        "Creates an Action that contains the selected keyframes of the selected bones, marks it as an asset"
-    )
-    bl_options = {"REGISTER", "UNDO"} 
-
-
-    @classmethod
-    def poll(cls, context: bpy.types.Context) -> bool:
-        return context.active_object is not None
-
-    def execute(self, context: bpy.types.Context) -> Set[str]:
-        new_action = bpy.data.actions.new("Asset")
-        src_action = context.object.animation_data.action
-        bone_names = {bone.name for bone in bpy.context.selected_pose_bones_from_active_object}
-        for bone_name in sorted(bone_names):
-            for location_index in range(3):
-                bone = bpy.context.object.pose.bones[bone_name]
-                rna_path = bone.path_from_id("location")
-                new_fcurve = new_action.fcurves.find(rna_path, index=location_index)
-                if new_fcurve is None:
-                    new_fcurve = new_action.fcurves.new(rna_path, index=location_index, action_group=bone_name)
-                src_fcurve = src_action.fcurves.find(rna_path, index=location_index)
-                for keyframe in src_fcurve.keyframe_points:
-                    new_fcurve.keyframe_points.insert(frame=keyframe.co.x, value=keyframe.co.y)
-                
-                new_action.asset_mark()
-                new_action.asset_generate_preview()
-        
-        return {'FINISHED'}
 
 def menu_func(self, context):
     self.layout.operator(CreateAnimationAsset.bl_idname, text=CreateAnimationAsset.bl_label)
